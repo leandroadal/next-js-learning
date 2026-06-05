050777f6d68cfd8dfd7ba2a8c070a278fd63614d
Leandro Silva <lleandro.adal@gmail.com>
2026-06-05 16:21:43 -0300

    feat: add header component

--- src/features/HomePage/index.tsx
+++ src/features/HomePage/index.tsx
@@ -1,5 +1,6 @@
 import { PostData } from '@/domain/posts';
 import { Container } from './styles';
+import { Header } from '@/components/layout/Header/Header';
 
 export type HomeProps = {
   posts: PostData[];
@@ -7,12 +8,15 @@ export type HomeProps = {
 
 export default async function HomePage({ posts }: HomeProps) {
   return (
-    <Container>
-      <div>
-        {posts.map((post) => (
-          <h2 key={post.slug}>{post.title}</h2>
-        ))}
-      </div>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        <div>
+          {posts.map((post) => (
+            <h2 key={post.slug}>{post.title}</h2>
+          ))}
+        </div>
+      </Container>
+    </>
   );
 }
