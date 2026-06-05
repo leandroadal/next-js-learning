86fbf7c8cbcee67cf4c1041a710b120838caa0b4
Leandro Silva <lleandro.adal@gmail.com>
2026-06-05 15:57:03 -0300

    style: update theme and GlobalStyles

--- src/styles/globals-styles.ts
+++ src/styles/globals-styles.ts
@@ -3,8 +3,34 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
+  }
+
   body {
     background: ${({ theme }) => theme.colors.background};
     color: ${({ theme }) => theme.colors.text};
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: ${({ theme }) => theme.font.sizes.medium};
+
+  }
+  a {
+    color: ${({ theme }) => theme.colors.secondary};
+    text-decoration: none;
+    transition: opacity 300ms ease-in-out;
+
+    &:hover {
+      opacity: .6;
+    }
   }
 `;
