a590208312b8f965eac8072fc1bf8edc2250ab0f
Leandro Silva <lleandro.adal@gmail.com>
2026-06-11 22:30:48 -0300

    chore: add Storybook configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import { defineConfig, globalIgnores } from 'eslint/config';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import nextVitals from 'eslint-config-next/core-web-vitals';
@@ -15,6 +18,7 @@ const eslintConfig = defineConfig([
     'next-env.d.ts',
   ]),
   eslintPluginPrettierRecommended,
+  ...storybook.configs['flat/recommended'],
 ]);
 
 export default eslintConfig;
